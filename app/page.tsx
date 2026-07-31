826de66ffbb73bfbaeea1a228d0fbeaf355175e9
mstechbaddie <mstechbaddie@gmail.com>
2026-07-31 21:47:43 +0100

    Restore Navbar and Cart

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Categories from "./components/Categories";
 import FeaturedProducts from "./components/FeaturedProducts";
+import Cart from "./components/Cart";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-white min-h-screen">
+      <Navbar />
       <Hero />
+      <Categories />
       <FeaturedProducts />
+      <Cart />
     </main>
   );
 }
